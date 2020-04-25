7e34ac42d68a607a0c96f7363da92dcf5b64fd39
John Martin <jrmartin182@gmail.com>
2020-04-25 17:57:07 -0600

    built card and card display

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+ 
+const Card = ({}) => {
+
+    return (
+        <div className="card">
+            <p id="#">#1</p>
+            <p className="term">Word</p>
+            <button>Flip Card</button>
+            
+        </div>
+    );
+}
+
+export default Card;
--- src/components/CardDisplay.js
+++ src/components/CardDisplay.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Card from './Card'
  
 class CardDisplay extends React.Component{
     constructor(props){
@@ -7,8 +8,10 @@ class CardDisplay extends React.Component{
 
     render(){
         return(
-
-        );
+            <div id="card_display">
+                <Card />
+            </div>
+        )
     };
 };
 

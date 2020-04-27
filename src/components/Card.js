79b37187ce7f91ea9945dc98d2a084cf5dee2565
John Martin <jrmartin182@gmail.com>
2020-04-27 16:37:41 -0600

    added flip effect to card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import '../comp-styling/card.css'
  
 const Card = ({card}) => {
 
     return (
         <div className="card">
-            <p id="#">#1</p>
-            <p className="term">Word</p>
+            <div className="card-inner">
+                <div className="card-front">
+                    <p>Test Front</p>
+                </div>
+
+                <div className="card-back">
+                    <p>Test Back</p>
+                </div>
             
+            </div>
         </div>
     );
 }

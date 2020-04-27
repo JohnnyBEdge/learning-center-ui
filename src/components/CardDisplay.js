db417fda840507869f28cd884c027ee6daf042c0
John Martin <jrmartin182@gmail.com>
2020-04-27 11:04:41 -0600

    connected GET to displaying card terms in a list

--- src/components/CardDisplay.js
+++ src/components/CardDisplay.js
@@ -5,13 +5,35 @@ import AddCardForm from './AddCardForm';
 class CardDisplay extends React.Component{
     constructor(props){
         super(props);
-    }
+
+        this.state = {
+            cards: []
+        }
+    };
+
+    getVocab = () => {
+        fetch(`http://localhost:5001/api/vocab`)
+            .then(response => response.json())
+            .then(data => this.setState({cards: data}, () => console.log(this.state.cards)))
+    };
+
+    componentDidMount(){
+        this.getVocab();
+    };
+
+    
 
     render(){
+        const displayVocab = this.state.cards.map((card) => {
+            return <li>{card.term}</li>
+        })
         return(
             <div id="card_display">
                 <AddCardForm />
                 <Card />
+                <ul>
+                    {displayVocab}
+                </ul>
             </div>
         )
     };
